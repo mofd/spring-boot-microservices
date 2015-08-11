a8fa06903952ddec7bd63387f5104a30cc4158c5
mofd <konstantin.steuer@email.de>
2015-08-11 18:33:38 +0200

    * CurrentTime aufgeräumt

--- current-time-app/src/main/java/org/example/microservices/currenttimeapp/CurrentTimeApp.java
+++ current-time-app/src/main/java/org/example/microservices/currenttimeapp/CurrentTimeApp.java
@@ -1,18 +1,10 @@
 package org.example.microservices.currenttimeapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 /**
  * TODO (KS) Dokumentieren.
